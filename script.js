0b29898a5dd9982ace7a54ffd31cff7d7367eefc
Prateek Lohani <prateeklohani105@gmail.com>
2023-12-22 17:56:08 +0530

    final

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ tl.to('#main',{
     opacity:1,
     duration:1
 })
+
+
+var form=document.querySelector('form');
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+        Toastify({
+            text: "Wohoo!! You are now a member.",
+            className: "button",
+            position:"center",
+    
+            duration: 3000,
+            gravity: "center",
+            backgroundColor: "orangered",
+            style: {
+              background: "orangered",
+              FontFace:"Helvetica",
+              
+            }
+          }).showToast();
+
+          document.querySelector('input').value='';
+    
+})
